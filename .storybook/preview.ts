7fc9d1d6593a2158f7a860f9b4dfc5bee3dd903f
Troy Chaplin <troy.chaplin@gmail.com>
2023-02-19 08:06:08 -0500

    Initial commit with Vite, Storybook and Tailwind CSS

---
+++ .storybook/preview.ts
@@ -0,0 +1,13 @@
+import "../src/index.css";
+export const parameters = {
+	backgrounds: {
+		default: "light",
+	},
+	actions: { argTypesRegex: "^on[A-Z].*" },
+	controls: {
+		matchers: {
+			color: /(background|color)$/i,
+			date: /Date$/,
+		},
+	},
+};
